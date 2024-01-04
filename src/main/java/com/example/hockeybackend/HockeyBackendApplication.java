fa9de5c66b6980912a1b39c45dc4cbc240f2324e
Shak789 <shakjivraj87@gmai.com>
2024-01-04 09:50:45 -0500

    initial commit

---
+++ src/main/java/com/example/hockeybackend/HockeyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hockeybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HockeyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hockeybackend/HockeyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hockeybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
